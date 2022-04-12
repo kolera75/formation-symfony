ecfb8c01a65311590c750abeb538fb8e94a46bda
kolera75 <amine.sebki@gmail.com>
2022-04-12 11:21:06 +0200

    Ajout de la création d'un livre

--- src/Controller/Admin/BookController.php
+++ src/Controller/Admin/BookController.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin;
 
+use App\Entity\Book;
+use App\Form\BookType;
 use App\Repository\BookRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 
 class BookController extends AbstractController
 {
@@ -22,6 +25,24 @@ class BookController extends AbstractController
 
 
     }
+
+    #[Route('/admin/livres/nouveau', name: 'app_admin_book_create', methods: ['GET', 'POST'])]
+	public function create(Request $request, BookRepository $repository): Response
+	{
+		$form = $this->createForm(BookType::class, new Book());
+
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			$repository->add($form->getData());
+
+			return $this->redirectToRoute('app_admin_book_list');
+		}
+
+		return $this->render('admin/book/create.html.twig', [
+			'form' => $form->createView(),
+		]);
+	}
 
     
     
