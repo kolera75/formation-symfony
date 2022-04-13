0aec53cd33e89f5fbafed21f980982aeb095f535
kolera75 <amine.sebki@gmail.com>
2022-04-13 14:26:39 +0200

    Affichage du formulaire de recherche & désactivation protection csrf

---
+++ src/Form/SearchBookType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Form;
+
+use App\DTO\BookSearchCriteria;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SearchBookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Par titre :',
+                'required' => false,
+            ])
+            ->add('authorName', TextType::class, [
+                'label' => 'Par nom d\'auteur :',
+                'required' => false,
+            ])
+            ->add('categoryName', TextType::class, [
+                'label' => 'Par nom de catégorie :',
+                'required' => false,
+            ])
+            ->add('limit', IntegerType::class, [
+                'label' => 'Nombre de résultat :',
+            ])
+            ->add('page', IntegerType::class, [
+                'label' => 'Page :',
+            ])
+            ->add('orderBy', ChoiceType::class, [
+                'label' => 'Trier par :',
+                'choices' => [
+                    'prix' => 'price',
+                    'identifiant' => 'id',
+                    'date de création' => 'createdAt',
+                    'date de mise à jour' => 'updatedAt',
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => BookSearchCriteria::class,
+            'method' => 'GET',
+            'csrf_protection' => false,
+        ]);
+    }
+
+    /**
+     * Désactive le prefix du formulaire, permettant d'avoir
+     * de belle urls.
+     */
+    public function getBlockPrefix(): string
+    {
+        return '';
+    }
+}
