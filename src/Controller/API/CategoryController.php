aabdfb917ef95be491ef49b24b9c21551f559536
kolera75 <amine.sebki@gmail.com>
2022-04-15 11:31:31 +0200

    Ajout de DELETE /categories/{id}

--- src/Controller/API/CategoryController.php
+++ src/Controller/API/CategoryController.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller\API;
 
+use App\Entity\Category;
+use App\Form\API\CategoryType;
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,7 +23,64 @@ class CategoryController extends AbstractController
 	}
 
 	#[Route('/api/categories', name: 'app_api_category_create', methods: ['POST'])]
-	public function create(): Response
+	public function create(Request $request, CategoryRepository $repository): Response
 	{
+		// Création du formulaire
+		$form = $this->createForm(CategoryType::class);
+
+		// On remplie le formulaire
+		$form->handleRequest($request);
+
+		// On test si le formulaire à un problème
+		if (!$form->isSubmitted() || !$form->isValid()) {
+			// Ici on affiche l'erreur
+			return $this->json($form->getErrors(true), 400);
+		}
+
+		// On enregistre la category en base de données
+		$repository->add($form->getData());
+
+		// On affiche la catégory en json
+		return $this->json($form->getData(), 201);
+	}
+
+	#[Route('/api/categories/{id}', name: 'app_api_category_get', methods: ['GET'])]
+	public function get(Category $category): Response
+	{
+		return $this->json($category);
+	}
+
+	#[Route('/api/categories/{id}', name: 'app_api_category_update', methods: ['PUT', 'PATCH'])]
+	public function update(
+		Category $category,
+		CategoryRepository $repository,
+		Request $request,
+	): Response {
+		// Création du formulaire
+		$form = $this->createForm(CategoryType::class, $category, [
+			'method' => $request->getMethod(),
+		]);
+
+		// On remplie le formulaire
+		$form->handleRequest($request);
+
+		// On test si le formulaire à une erreur
+		if (!$form->isSubmitted() || !$form->isValid()) {
+			return $this->json($form->getErrors(true), 400);
+		}
+
+		// On enregistre la catégorie
+		$repository->add($form->getData());
+
+		// On affiche la catégorie en JSON
+		return $this->json($form->getData());
+	}
+
+	#[Route('/api/categories/{id}', name: 'app_api_category_delete', methods: ['DELETE'])]
+	public function delete(Category $category, CategoryRepository $repository): Response
+	{
+		$repository->remove($category);
+
+		return $this->json($category);
 	}
 }
